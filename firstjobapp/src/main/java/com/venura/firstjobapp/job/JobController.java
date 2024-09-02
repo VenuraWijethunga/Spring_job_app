db61098bf8488e1661d92a4f792ec1c4ca4db7dd
VenuraWijethunga <venurapavan73@gmail.com>
2024-09-02 16:29:46 +0530

    Update job using the ID

--- firstjobapp/src/main/java/com/venura/firstjobapp/job/JobController.java
+++ firstjobapp/src/main/java/com/venura/firstjobapp/job/JobController.java
@@ -43,6 +43,14 @@ public class JobController {
                 return new ResponseEntity<>("Job deleted Succssfully", HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+@PutMapping("/jobs/{id}")
+    public ResponseEntity<String> updateJob(@PathVariable Long id,
+                                            @RequestBody Job updatedJob){
+        boolean updated = jobService.updateJob(id, updatedJob);
+        if(updated)
+            return new ResponseEntity<>("Job updated succesfully",HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
 
 
